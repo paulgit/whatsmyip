85541f24f1daaccb064a2af7506809ba160165fc
Paul Git <paulgit@pm.me>
2020-04-01 14:05:35 +0200

    Enhanced script to allow different formats to be output.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # whatsmyip
-Simple PHP Script to return your true external ip, even works around proxies.
+This is a simple PHP script to return your true external ip address, it even works around proxies. This was forked from [Testo Development's script](https://github.com/TestoEXE/whatsmyip).
+
+I have enhanced the original script by allowing an additional format specifier. By default an html version of your IP address will be displayed on your screen. This is formated to be large on both desktop and mobile devices. I have no idea if the CSS is valid as I find CSS to be voodoo, so any suggestions of improvements would be welcome. Other formats available are text and JSON.
+
+## Example usage
+
+### html format
+* https://example.org
+* https://example.org/?format=html
+
+### text format
+* https://example.org/?format=html
+
+### json format
+* https://example.org/?format=html
+
+You can see the script in action at [https://ip.poyo.ch](https://ip.poyo.ch)
--- index.php
+++ index.php
@@ -1,20 +1,65 @@
 <?php
+/*
+ * What's My IP Script.
+ * PHP Version 5.x,7.x.
+ *
+ * @see       https://code.paulg.it/paulgit/whatsmyip What's My IP
+ *
+ * @author    Testo Development (TestoEXE)
+ * @author    Paul Git (paulgit) <paulgit@pm.me>
+ * @copyright 2019 Testo Development
+ * @copyright 2020 Paul Git
+ * @license   MIT License
+ */
+
+// Common headers
+header('Vary: Origin');
+header('Cache-Control: private, no-cache');
+
 function whatsMyIP()
 { 
-    foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key)
-    {
-        if (array_key_exists($key, $_SERVER) === true)
-        {
-            foreach (array_map('trim', explode(',', $_SERVER[$key])) as $ip)
-            {
-                if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false)
-                {
-                    return $ip;
-                }
-            }
-        }
-    }
+	foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key)
+	{
+		if (array_key_exists($key, $_SERVER) === true)
+		{
+			foreach (array_map('trim', explode(',', $_SERVER[$key])) as $ip)
+			{
+				if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false)
+				{
+					return $ip;
+				}
+			}
+		}
+	}
 }
+
 $users_ip = whatsMyIP();
-echo $users_ip; // Output IP address [Ex: 123.12.41.42]
+
+if (isset($_GET["format"]))
+{
+	$format = trim($_GET["format"]);
+}
+else
+{
+	$format = "html";
+}
+
+if ($format == "text")
+{
+	header('Content-Type: text/plain');
+	echo $users_ip;
+}
+else if ($format == "json")
+{
+	header('Content-Type: application/json');
+	echo "{\"ip\":\"$users_ip\"}";
+}
+else
+{
+	header("Content-Type: text/html");
+	echo "<html><body><div style=\"display: flex;flex-direction: column;justify-content: center;align-items: center;text-align: center;min-height: 50vh;font-family:monospace;font-size:600%;\">";
+	echo $users_ip;
+	echo "</div></body></html>";
+}
+
 ?>
